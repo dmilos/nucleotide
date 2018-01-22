531ce300dcfa073e11b9c87e55fad75362e19446
Dejan Milosavljevic <dmilos@email.com>
2018-01-22 10:21:20 +0100

    * enhance: [internal] Possible idea for include

--- tmp/include-idea.py
+++ tmp/include-idea.py
@@ -1,7 +1,22 @@
 settings.accumulate(
+    'include-single',
+        {
+         "/what/ever"
+        }
+    )
+
+settings.accumulate(
+    'include-list', # same as 'include'
+          [ "/what/ever01", "/what/ever02" ]
+    )
+
+settings.accumulate(
     'include-switch',
         {
-            "list" : { 'version10': [ '/folder/directorium/v10'  ], 'version20': [ '/folder/directorium/v20'  ] }
-            ,"active": "version20"
+             'option' : {    'version10': [ '/folder/directorium/v10' ]
+                           , 'version20': [ '/folder/directorium/v20' ]
+                           , 'version30': [ '/folder/directorium/v30' ]
+                        }
+            ,'active': "version20"
         }
     )
